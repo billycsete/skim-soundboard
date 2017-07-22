261a7b3b31316380846348b98b1878c4007ca0aa
Billy Csete <wcsete@apple.com>
2017-07-21 17:00:34 -0700

    feat(): add breakpoints and grid

--- src/js/main.js
+++ src/js/main.js
@@ -11,5 +11,3 @@ const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 window.addEventListener('keydown', playSound);
-
-// console.log('it works');
